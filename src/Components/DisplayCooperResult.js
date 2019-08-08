8605db35e8f5e391667249c707f237fb15baf3f5
pedro_bras <pedro.c.bras@gmail.com>
2019-08-08 13:23:28 +0200

    updates display result component to render save button if props authenticated

--- src/Components/DisplayCooperResult.js
+++ src/Components/DisplayCooperResult.js
@@ -12,15 +12,27 @@ class DisplayCooperResult extends Component {
 
   render() {
     let results;
+    let saveButton;
+
+    if (this.props.authenticated === true) {
+      saveButton = (
+        <>
+          <button id="save-result" onClick={this.saveCooperData.bind(this)}>
+            Save entry
+          </button>
+        </>
+      );
+    }
     if (this.props.age !== "" && this.props.distance !== "") {
       results = (
-        <div>
+        <>
           <p>
             {this.props.age} y/o {this.props.gender} running{" "}
             {this.props.distance} meters.
           </p>
           <p>Result: {this.calculate()}</p>
-        </div>
+          {saveButton}
+        </>
       );
     }
     return <div>{results}</div>;
