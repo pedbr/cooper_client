64116f860bbff5d616971dfc62401cea28e8331a
pedro_bras <pedro.c.bras@gmail.com>
2019-08-08 10:58:05 +0200

    adds login form component to app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import DisplayCooperResult from "./Components/DisplayCooperResult";
 import InputFields from "./Components/InputFields";
+import LoginForm from "./Components/LoginForm";
 import "./App.css";
 
 class App extends Component {
@@ -25,6 +26,7 @@ class App extends Component {
         <InputFields inputChangeHandler={this.onChange.bind(this)} />
 
         <button id="login">Login</button>
+        <LoginForm />
 
         <DisplayCooperResult
           distance={this.state.distance}
