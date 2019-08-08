a75de5701b4b9a934691001f27195e107c604fe8
pedro_bras <pedro.c.bras@gmail.com>
2019-08-08 10:33:16 +0200

    adds logic to diplay result component

--- src/Components/DisplayCooperResult.js
+++ src/Components/DisplayCooperResult.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import CooperCalculator from "../Modules/CooperCalculator";
 
 class DisplayCooperResult extends Component {
+  calculate() {
+    return CooperCalculator(
+      this.props.distance,
+      this.props.gender,
+      this.props.age
+    );
+  }
+
   render() {
-    return <div />;
+    let results;
+    if (this.props.age !== "" && this.props.distance !== "") {
+      results = (
+        <div>
+          <p>
+            {this.props.age} y/o {this.props.gender} running{" "}
+            {this.props.distance} meters.
+          </p>
+          <p>Result: {this.calculate()}</p>
+        </div>
+      );
+    }
+    return <div>{results}</div>;
   }
 }
 
