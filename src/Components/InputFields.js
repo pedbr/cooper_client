521f6fe7bd78534a3c0f7fb47a6b9865c3b9b6b0
pedro_bras <pedro.c.bras@gmail.com>
2019-08-08 17:09:24 +0200

    styles input data box

--- src/Components/InputFields.js
+++ src/Components/InputFields.js
@@ -2,18 +2,40 @@ import React from "react";
 
 const InputFields = props => {
   return (
-    <>
-      <label>Distance</label>
-      <input id="distance" onChange={props.inputChangeHandler} />
-
-      <select id="gender" onChange={props.inputChangeHandler}>
-        <option value="female">Female</option>
-        <option value="male">Male</option>
-      </select>
-
-      <label>Age</label>
-      <input id="age" onChange={props.inputChangeHandler} />
-    </>
+    <div className="input-data-div">
+      <div className="input-data-form">
+        <div className="input-data-element">
+          <label>Distance</label>
+        </div>
+        <div className="input-data-element">
+          <input
+            className="input-box"
+            id="distance"
+            onChange={props.inputChangeHandler}
+          />
+        </div>
+        <div className="input-data-element">
+          <select
+            className="input-box"
+            id="gender"
+            onChange={props.inputChangeHandler}
+          >
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </select>
+        </div>
+        <div className="input-data-element">
+          <label>Age</label>
+        </div>
+        <div className="input-data-element">
+          <input
+            className="input-box"
+            id="age"
+            onChange={props.inputChangeHandler}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
